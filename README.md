911a6880f15de4a731fe494a6b009278586ba9b0
Devendra Goswami <97433626+pdevendragoswami@users.noreply.github.com>
2024-01-23 20:45:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+1. create folder for your project
+2. create env for your project - conda create -n TextSummarizer python=3.8 -y
+3. activate env - conda activate TextSummarizer
+4. create template.py file - echo. > template.py
+5. define the project template and run the file - python template.py
+6. define the requirements.txt and setup.py file
+7. run the requirements.txt file  - pip install -r requirements.txt
+8. Define the constant path in constants-__init__.py file
+9. Define logger function in logging folder
+10. Define the utils functions that are commonly used.
+11. Define config.yaml and params.yaml file for stage-01-data_ingestion
+12. Write code from stage 01 data ingestion in notebook
+13. Define config_entity.py file and configuration.py for the stage 01
+14. write modular code for stage 01 with referece from notebook for component and pipeline
+15. Repeat step from 11 to 15 for each step.
+
+
---
+++ template.py
@@ -0,0 +1,71 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "TextSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/stage_01_data_ingestion.py",
+    f"src/{project_name}/components/stage_02_data_validation.py",
+    f"src/{project_name}/components/stage_03_data_transformation.py",
+    f"src/{project_name}/components/stage_04_model_trainer.py",
+    f"src/{project_name}/components/stage_05_model_evaluation.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/utils.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py",
+    f"src/{project_name}/pipeline/stage_02_data_validation.py",
+    f"src/{project_name}/pipeline/stage_03_data_transformation.py",
+    f"src/{project_name}/pipeline/stage_04_model_trainer.py",
+    f"src/{project_name}/pipeline/stage_05_model_evaluation.py", 
+    f"src/{project_name}/pipeline/prediction.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py", 
+    f"src/{project_name}/constants/__init__.py",
+    "research/trials.ipynb",
+    "research/stage_01_data_ingestion.ipynb",
+    "research/stage_02_data_validation.ipynb",
+    "research/stage_03_data_transformation.ipynb",
+    "research/stage_04_model_trainer.ipynb",
+    "research/stage_05_model_evaluation.ipynb",
+    f"research/{project_name}.ipynb",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "test.py",
+    "README.md"
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists") 
